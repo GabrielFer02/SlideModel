eedc3467aa33fcd16d9c6536c6bcc7e64607ad3b
Gabriel Fernando <gf313074@gmail.com>
2024-09-25 23:47:29 -0300

    add custom buttons

--- js/slide.js
+++ js/slide.js
@@ -157,7 +157,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slideContainer, slide) {
     super(slideContainer, slide);
     this.bindControlEvents();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import {SlideNav} from "../js/slide.js";
+import SlideNav from "../js/slide.js";
 
 const slide = new SlideNav(".slide-container", ".slide");
 slide.init();
 slide.addArrow(".prev", ".next");
-slide.addControl();
+slide.addControl(".custom-controls");
